8d5629b9ac4cac50afe7e95e822447747d5ad351
racksoup <rack.connor1@gmail.com>
2023-09-05 19:04:09 -0400

    f

--- server.js
+++ server.js
@@ -22,3 +22,7 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 ChangeLightOnTimeOfDay();
 
 // curl http://192.168.2.42:42000/api/connor-bedroom-light
+
+//huebridge 192.168.2.11
+//pi 192.168.2.10
+//comp 192.168.2.15
