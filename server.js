cdec232e79c667022bb32962a700399194668b3f
racksoup <rack.connor1@gmail.com>
2025-02-18 03:18:06 -0500

    cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require('cors');
 const {
   ChangeLightOnTimeOfDay,
 } = require("./middleware/ChangeLightOnTimeOfDay");
@@ -8,6 +9,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(
